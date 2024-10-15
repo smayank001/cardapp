2e7a8f2faab7a2ea75127d982c935b4bb14b10d8
Mayank saxena <mayanksaxena1262@gmail.com>
2024-10-15 19:00:30 +0530

    Add Header.js component and other changes

---
+++ src/Component/Header.js
@@ -0,0 +1,15 @@
+//import logo from "./logo.svg";
+import "../App.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Task Manager</h1>
+      <div className="btnWrapper">
+        <button>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
